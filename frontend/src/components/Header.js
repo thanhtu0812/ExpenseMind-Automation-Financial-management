be107391884006042d7585b2fea6cf8d9e9ac456
thuynguyen0120nt <thuynguyen0120nt@gmail.com>
2025-11-19 16:15:18 +0700

    Update Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -89,26 +89,12 @@ const Header = () => {
     <header className="app-header">
       <div className="header-left">
         <h2 className="header-logo">ExpenseMind</h2>
-        {isLoggedIn && (
-          <div className="header-search">
-            <input 
-              type="text" 
-              placeholder="Search..." 
-              className="header-search-input"
-            />
-          </div>
-        )}
+        
       </div>
 
       {isLoggedIn ? (
         <div className="header-right">
-          <button className="header-icon-btn notification-btn">
-            <svg width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-              <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"/>
-              <path d="M13.73 21a2 2 0 0 1-3.46 0"/>
-            </svg>
-            <span className="notification-badge"></span>
-          </button>
+    
           <div className="header-avatar-wrapper">
             <button className="header-avatar-btn" onClick={toggleDropdown}>
               <img src={userAvatar} alt="User" className="header-avatar" />
